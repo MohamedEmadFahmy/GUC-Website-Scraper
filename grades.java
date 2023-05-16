02fcd1a565194fcdce01c6953361c796c0eb12ef
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-05-16 16:54:24 +0200

    temp fix

--- grades.java
+++ grades.java
@@ -3,21 +3,16 @@
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
-import java.io.OutputStream;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.service.DriverService;
 import org.openqa.selenium.support.ui.Select;
 
 public class grades {
@@ -26,15 +21,15 @@ public class grades {
     private static String quizTableID = "UnFormMainContent_nttTr";
     private static String midtermTableID = "UnFormMainContent_midDg";
     private static String gradeLink = "https://apps.guc.edu.eg//student_ext/Grade/CheckGrade.aspx";
+    static String result = "";
 
     public static void main(String[] args) {
         // System.setProperty("webdriver.chrome.driver", "C:/Program Files
         // (x86)/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-
         // Configuring options for driver
         ChromeOptions options = new ChromeOptions();
-        // options.addArguments("headless"); // setting headless mode to true.. so there
+        options.addArguments("headless"); // setting headless mode to true.. so there
         // isn't any ui
         options.addArguments("log-level=1"); // sets log level to display only error logs
 
@@ -46,6 +41,9 @@ public class grades {
         String username = Credentials[0];
         String password = Credentials[1];
 
+        System.out.print("\033[H\033[2J");
+        // System.out.flush();
+
         // Navigate to a webpage
         driver.get("https://" + username + ":" + password + "@student.guc.edu.eg/");
         driver.get("https://" + username + ":" + password + "@apps.guc.edu.eg/student_ext/Default.aspx");
@@ -53,13 +51,18 @@ public class grades {
         getGrades(driver);
         System.out.println();
         System.out.println("-------------Midterms-------------");
+        result += "\n";
+        result += "\n" + "---------------Midterms--------------";
         getMidterms(driver);
 
         System.out.println();
         System.out.println();
 
-        System.out.println("Grade fetch done");
         driver.quit(); // Close the webdriver
+        System.out.println("Grade fetch done");
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println(result);
 
         // Keep the program running until user presses ENTER button
         System.out.println("Press ENTER to quit program");
@@ -119,6 +122,8 @@ public class grades {
         int numCourses = options.size();
         String courseNames[] = new String[numCourses];
 
+        result += "\n";
+        result += "\n";
         System.out.println();
         System.out.println();
 
@@ -135,11 +140,13 @@ public class grades {
             dropdown = new Select(selectCourse);
             dropdown.selectByVisibleText(courseNames[i] + "");
             System.out.println(i + ") " + courseNames[i]);
+            result += (i + ") " + courseNames[i]);
 
             WebElement gradesTable = null;
 
             try {
                 System.out.println();
+                result += "\n";
                 gradesTable = driver.findElement(By.id(quizTableID));
                 printTable(gradesTable, "quiz");
 
@@ -150,6 +157,9 @@ public class grades {
             System.out.println();
             System.out.println("----------------------------------");
             System.out.println();
+            result += "\n";
+            result += "----------------------------------";
+            result += "\n";
         }
     }
 
@@ -159,21 +169,26 @@ public class grades {
         for (int i = 1; i < rowsList.size(); i++) {
             WebElement row = rowsList.get(i);
             System.out.println();
+            result += "\n";
             columnsList = row.findElements(By.tagName("td"));
             for (int j = 0; j < columnsList.size(); j++) {
                 WebElement column = columnsList.get(j);
                 if (j != columnsList.size() - 1 || !typeTable.equals("quiz")) {
                     // System.out.print(column.getText() + " | ");
                     System.out.print(column.getText());
+                    result += (column.getText());
                     if (j != columnsList.size() - 1) {
                         System.out.print("     ");
+                        result += ("     ");
                     }
                 }
                 if (typeTable.equals("midterm") && j == columnsList.size() - 1) {
                     System.out.print("%");
+                    result += ("%");
                 }
             }
             System.out.println();
+            result += "\n";
         }
     }
 
